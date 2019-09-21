0cb92db2bdee05c548b770e41e461614454461e5
Michael Dunn <michaelsdunn1@gmail.com>
2019-09-21 16:29:28 -0500

    Fixed Ticker datatype.

--- src/lib/CoinbasePro/WebSocketFeed/Channel/Ticker.hs
+++ src/lib/CoinbasePro/WebSocketFeed/Channel/Ticker.hs
@@ -5,32 +5,25 @@ module CoinbasePro.WebSocketFeed.Channel.Ticker
     ( Ticker (..)
     ) where
 
-import           Data.Aeson.TH     (defaultOptions, deriveJSON)
+import           Data.Aeson.Casing (snakeCase)
+import           Data.Aeson.TH     (defaultOptions, deriveJSON,
+                                    fieldLabelModifier)
 import           Data.Time.Clock   (UTCTime)
 
 import           CoinbasePro.Types (Price, ProductId, Sequence, Side, Size)
 
 
-type Volume24h = Double
-type Volume30d = Double
-
-
 data Ticker = Ticker
-    { sequence  :: Sequence
+    { tradeId   :: Maybe Int
+    , sequence  :: Sequence
+    , time      :: Maybe UTCTime
     , productId :: ProductId
     , price     :: Price
-    , openPrice :: Price -- last 24h
-    , volume24h :: Volume24h -- last 24h
-    , lowPrice  :: Price -- last 24h
-    , highPrice :: Price -- last 24h
-    , volume30d :: Volume30d
+    , side      :: Maybe Side
+    , lastSize  :: Maybe Size
     , bestBid   :: Price
     , bestAsk   :: Price
-    , side      :: Maybe Side
-    , time      :: Maybe UTCTime
-    , tradeId   :: Maybe Int
-    , lastSize  :: Maybe Size
     } deriving (Eq, Ord, Show)
 
 
-deriveJSON defaultOptions ''Ticker
+deriveJSON defaultOptions {fieldLabelModifier = snakeCase} ''Ticker
