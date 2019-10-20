2ecae19db0c49747827f44de27b91a998cf4b6f7
Michael Dunn <michaelsdunn1@gmail.com>
2019-10-20 12:51:57 -0500

    Unauthenticated requests are now in the ClientM monad instead of IO.

--- src/lib/CoinbasePro/Unauthenticated.hs
+++ src/lib/CoinbasePro/Unauthenticated.hs
@@ -13,6 +13,7 @@ module CoinbasePro.Unauthenticated
    ) where
 
 import           Data.Time.Clock                           (UTCTime)
+import           Servant.Client                            (ClientM)
 
 import           CoinbasePro.Headers                       (userAgent)
 import           CoinbasePro.MarketData.AggregateOrderBook (AggregateOrderBook)
@@ -30,40 +31,40 @@ import qualified CoinbasePro.Unauthenticated.API           as API
 
 
 -- | https://docs.pro.coinbase.com/#get-products
-products :: IO [Product]
-products = run $ API.products userAgent
+products :: ClientM [Product]
+products = API.products userAgent
 
 
 -- | https://docs.pro.coinbase.com/#get-product-order-book
-aggregateOrderBook :: ProductId -> Maybe AggregateBookLevel -> IO AggregateOrderBook
-aggregateOrderBook prid agg = run $ API.aggregateOrderBook prid agg userAgent
+aggregateOrderBook :: ProductId -> Maybe AggregateBookLevel -> ClientM AggregateOrderBook
+aggregateOrderBook prid agg = API.aggregateOrderBook prid agg userAgent
 
 
 -- | https://docs.pro.coinbase.com/#get-product-order-book
-fullOrderBook :: ProductId -> IO FullOrderBook
-fullOrderBook prid = run $ API.fullOrderBook prid (Just FullBookLevel) userAgent
+fullOrderBook :: ProductId -> ClientM FullOrderBook
+fullOrderBook prid = API.fullOrderBook prid (Just FullBookLevel) userAgent
 
 
 -- | https://docs.pro.coinbase.com/#get-trades
-trades :: ProductId -> IO [Trade]
-trades prid = run $ API.trades prid userAgent
+trades :: ProductId -> ClientM [Trade]
+trades prid = API.trades prid userAgent
 
 
 -- | https://docs.pro.coinbase.com/#get-historic-rates
-candles :: ProductId -> Maybe UTCTime -> Maybe UTCTime -> CandleGranularity -> IO [Candle]
-candles prid start end cg = run $ API.candles prid start end cg userAgent
+candles :: ProductId -> Maybe UTCTime -> Maybe UTCTime -> CandleGranularity -> ClientM [Candle]
+candles prid start end cg = API.candles prid start end cg userAgent
 
 
 -- | https://docs.pro.coinbase.com/#get-24hr-stats
-stats :: ProductId -> IO TwentyFourHourStats
-stats prid = run $ API.stats prid userAgent
+stats :: ProductId -> ClientM TwentyFourHourStats
+stats prid = API.stats prid userAgent
 
 
 -- | https://docs.pro.coinbase.com/?javascript#get-currencies
-currencies :: IO [Currency]
-currencies = run $ API.currencies userAgent
+currencies :: ClientM [Currency]
+currencies = API.currencies userAgent
 
 
 -- | https://docs.pro.coinbase.com/#time
-time :: IO CBTime
-time = run $ API.time userAgent
+time :: ClientM CBTime
+time = API.time userAgent
