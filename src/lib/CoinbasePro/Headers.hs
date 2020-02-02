821e5461d1ad6794bc294c2a6ffcd9504974256c
Michael Dunn <michaelsdunn1@gmail.com>
2020-02-02 15:38:50 -0600

    Updated version to 0.8.0.0.

--- src/lib/CoinbasePro/Headers.hs
+++ src/lib/CoinbasePro/Headers.hs
@@ -24,7 +24,7 @@ newtype UserAgent = UserAgent Text
 
 
 userAgent :: UserAgent
-userAgent = UserAgent "coinbase-pro/0.7"
+userAgent = UserAgent "coinbase-pro/0.8"
 
 
 type UserAgentHeader = RequiredHeader "User-Agent" UserAgent
